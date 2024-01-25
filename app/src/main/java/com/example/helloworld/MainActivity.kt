40c0d868d045a0991ce227bf8d78917580468ee2
kalin1233 <kalin32@bu.edu>
2024-01-25 12:34:34 -0500

    Added Hello Button

--- app/src/main/java/com/example/helloworld/MainActivity.kt
+++ app/src/main/java/com/example/helloworld/MainActivity.kt
@@ -2,10 +2,7 @@ package com.example.helloworld
 
 import android.os.Bundle
 import androidx.activity.ComponentActivity
-import androidx.activity.compose.setContent
-import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
-import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
@@ -14,25 +11,31 @@ import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import com.example.helloworld.ui.theme.HelloWorldTheme
+import android.view.View
+import android.widget.Button
+import android.widget.TextView
+
+
 
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContent {
-            HelloWorldTheme {
-                // A surface container using the 'background' color from the theme
-                Surface(
-                    modifier = Modifier.fillMaxSize(),
-                    color = MaterialTheme.colorScheme.background
-                ) {
-                    Greeting("Android")
-                }
+        setContentView(R.layout.activity_main)
+
+        val sayHelloButton = findViewById<Button>(R.id.sayHelloButton)
+        val helloWorldTextView = findViewById<TextView>(R.id.helloWorldTextView)
+
+        // Use View.OnClickListener to handle button click
+        sayHelloButton.setOnClickListener(object : View.OnClickListener {
+            override fun onClick(view: View) {
+                // Toggle visibility of "Hello World" text
+                helloWorldTextView.setText(R.string.hello_world_placeholder)
+                helloWorldTextView.visibility = View.VISIBLE
             }
-        }
+        })
     }
 }
-
 @Composable
 fun Greeting(name: String, modifier: Modifier = Modifier) {
     Surface(color = Color.Blue) {
